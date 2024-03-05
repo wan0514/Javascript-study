c86e7c6ce0b5321d0663e93cccbc445a175dedb7
wan <conut22@naver.com>
2024-03-05 21:59:15 +0900

    참조 타입에 객체유형 예시 추가

--- study/12-Type/primitive-type.js
+++ study/12-Type/primitive-type.js
@@ -10,6 +10,8 @@ const primitiveType = ['number', 'bigint', 'boolean', 'undefined', 'symbol'];
 let a = 100;
 let b = a;
 
+console.log(a === b); // true
+
 a = 50;
 
 //위와 같이 작성하면 a,b 값 모두 50으로 변할 것 같지만 아니다.
@@ -21,3 +23,5 @@ console.log('b: ', b); //b:  100
 // b에 a를 할당할 시 b: 100 이란 메모리가 생성된다.
 // a에 50을 재 할당 시 a:100 메모리는 여전히 존재하며, a: 50이라는 메모리가 새로 생성이되며, 새로 생긴 메모리를 가리키게 된다.
 // b는 여전히 b: 100이란 메모리를 가리키고 있으므로 100이란 값을 갖게 된다.
+
+console.log('a 변경된 후', a === b); // 그래서 이 결과는 false다
--- study/12-Type/reference-type.js
+++ study/12-Type/reference-type.js
@@ -4,9 +4,14 @@
 const referenceType = ['object', 'array', 'function'];
 
 //참조 타입은 메모리에 저장된 값을 가리키는 것이 아니라, 메모리에 저장된 값의 주소를 가리킨다.
-// 참조타입  -> call stack( 주소 ) ->  Heap (메모리 값)
+// 참조타입  -> call stack( heap을 가리키는 주소 ) ->  Heap (메모리 값)
 
-//example
+//example 1 ( array )
+
+// myArr를 선언하고 배열을 할당했다.
+// newArr에 myArr를 할당함으로서 메모리 값을 가리키는 주소가 같아진다.
+// 주소가 같기 때문에 myArr를 통해 배열을 수정하면 해당 주소에 해당하는 값이 바뀌기 때문에
+// 같은 주소를 참조하고 있는 newArr도 수정된다.
 
 let myArr = [1, 2, 3, 4];
 let newArr = myArr;
@@ -21,3 +26,30 @@ console.log('newArr', newArr);
 // 참조타입은 불변성을 갖고 있지 않다.
 // 따라서 기존 데이터를 변경해버릴 수 있는 위험이 있기 때문에 함부로 값을 변경하면 안된다.
 // 의도치 않은 사이드 이팩트를 발생시킬 수 있기 떄문이다. (ex: 같은 변수를 사용하는 다른 곳에서도 값이 변경 된다던가)
+
+// example 2 (object)
+
+const myObj = { name: 'bob', age: 50 };
+
+const newObj = myObj;
+
+console.log('myObj === newObj: ', newObj === myObj); //true
+
+myObj.name = 'milly';
+
+console.log('newObj: ', newObj); // { name: 'new object', age: 50 }
+
+// 그렇다면 어떻게 해야하는가?
+// 참조를 끊어주면 된다.
+// 참조를 끊기 위해서는 spread,map,filter,slice,reduce 와 같은 매서드를 사용해주면 된다.
+
+const myObj2 = { name: 'bob', age: 50 };
+
+const newObj2 = { ...myObj };
+
+console.log('myObj2 === newObj2: ', myObj2 === newObj2); // false :참조하는 객체가 다르다.
+
+newObj2.name = 'wario';
+
+console.log('newObj2: ', newObj2); // spread 문법을 사용해서 새로운 객체로 만들었기 때문에 같은 객체를 가리키고 있지 않다. 참조가 끊겼다. 원본 배열이 수정되지 않는다.
+console.log('myObj: ', myObj); // 기존 값은 그대로 유지한다.
